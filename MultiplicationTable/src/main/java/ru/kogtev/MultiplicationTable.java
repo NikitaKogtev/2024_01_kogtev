72f50d6bfdda2102f1ad41f7f4de01bbf606815c
Когтев Никита Сергеевич <kogtevns@gmail.com>
2024-03-16 20:55:18 +0700

    Написан вывод таблицы умножения

--- MultiplicationTable/src/main/java/ru/kogtev/MultiplicationTable.java
+++ MultiplicationTable/src/main/java/ru/kogtev/MultiplicationTable.java
@@ -7,39 +7,45 @@ import java.io.InputStreamReader;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        try {
+        int size = 10;
 
-            System.out.println("Введите размер таблицы умножения (от 1 до 32): ");
-
-            while (true) {
-
-                int size = Integer.parseInt(reader.readLine());
-
-                if (size >= 1 && size <= 32) {
-
-                    int minWidthValue = String.valueOf(size).length();
-                    int maxWidthValue = String.valueOf(size * size).length();
+        int minWidthValue = (size + "|").length();
+        int maxWidthValue = ((size * size) + "|").length();
 
 
-                    for (int i = 1; i <= size; i++) {
-                        for (int j = 1; j <= size; j++) {
-                            System.out.print(i * j);
-                            System.out.print(" ");
-                        }
-                        System.out.println(" ");
-                    }
+        // Выводим шапку таблицы
+        System.out.print(String.format("%" + (minWidthValue - 1) + "s|", ""));
+        for (int i = 1; i <= size; i++) {
+            System.out.print(String.format("%" + (maxWidthValue - 1) + "d|", i));
+        }
+        System.out.println();
 
-
-                } else {
-                    System.out.println("Вы ввели данные некорректно, значения должно быть от 1 до 32");
-                }
-
+        // Выводим разделитель
+        System.out.print(String.format("%" + minWidthValue + "s", "+").replace(" ", "-"));
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j < maxWidthValue; j++) {
+                System.out.print("-");
             }
+            System.out.print("+");
+        }
+        System.out.println();
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        //Выводим таблицу
+        for (int i = 1; i <= size; i++) {
+            System.out.print(String.format("%" + (minWidthValue - 1) + "d|", i));
+            for (int j = 1; j <= size; j++) {
+                System.out.print(String.format("%" + (maxWidthValue - 1) + "d|", i * j));
+            }
+            System.out.println();
+            System.out.print(String.format("%" + minWidthValue + "s", "+").replace(" ", "-"));
+            for (int k = 1; k <= size; k++) {
+                for (int j = 1; j < maxWidthValue; j++) {
+                    System.out.print("-");
+                }
+                System.out.print("+");
+            }
+            System.out.println();
         }
 
     }
